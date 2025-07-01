80babb19352beec70e59f02ca248bfe18205682b
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2025-07-01 22:47:41 +0530

    added timer to focus mode and proper lockdown

--- app/src/main/java/com/guruswarupa/launch/FocusMode.kt
+++ app/src/main/java/com/guruswarupa/launch/FocusMode.kt
@@ -5,12 +5,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.ResolveInfo
 
-data class FocusModeConfig(
-    val isEnabled: Boolean = false,
-    val allowedApps: Set<String> = emptySet()
-)
-
-class FocusModeManager(private val context: Context, private val sharedPreferences: SharedPreferences) {
+class FocusModeManager(private val sharedPreferences: SharedPreferences) {
 
     companion object {
         private const val FOCUS_MODE_ENABLED = "focus_mode_enabled"
@@ -21,10 +16,6 @@ class FocusModeManager(private val context: Context, private val sharedPreferenc
         return sharedPreferences.getBoolean(FOCUS_MODE_ENABLED, false)
     }
 
-    fun toggleFocusMode() {
-        val currentState = isFocusModeEnabled()
-        sharedPreferences.edit().putBoolean(FOCUS_MODE_ENABLED, !currentState).apply()
-    }
 
     fun getAllowedApps(): Set<String> {
         return sharedPreferences.getStringSet(FOCUS_MODE_ALLOWED_APPS, emptySet()) ?: emptySet()
@@ -44,22 +35,5 @@ class FocusModeManager(private val context: Context, private val sharedPreferenc
         val currentApps = getAllowedApps().toMutableSet()
         currentApps.remove(packageName)
         setAllowedApps(currentApps)
-    }
-
-    fun isAppAllowedInFocusMode(packageName: String): Boolean {
-        return if (isFocusModeEnabled()) {
-            getAllowedApps().contains(packageName)
-        } else {
-            true // All apps allowed in normal mode
-        }
-    }
-
-    fun filterAppsForFocusMode(apps: List<ResolveInfo>): List<ResolveInfo> {
-        return if (isFocusModeEnabled()) {
-            val allowedApps = getAllowedApps()
-            apps.filter { allowedApps.contains(it.activityInfo.packageName) }
-        } else {
-            apps
-        }
     }
 }
