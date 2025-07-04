e20d414ddd1ccf6e19d2da18d31f40a93208da9e
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2025-07-04 17:51:01 +0530

    release 4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "4.2"
+        versionName = "4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
