7596067f5418e4069131099704a07f1561d33b39
Guru Swarupa <msgswarupa@gmail.com>
2026-01-15 20:17:33 +0530

    optimize app

--- app/src/main/java/com/guruswarupa/launch/AppUsageMonitor.kt
+++ app/src/main/java/com/guruswarupa/launch/AppUsageMonitor.kt
@@ -49,13 +49,26 @@ class AppUsageMonitor : Service() {
     }
 
     private fun getForegroundApp(): String? {
-        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val tasks = activityManager.getRunningTasks(1)
-
-        return if (tasks.isNotEmpty()) {
-            tasks[0].topActivity?.packageName
-        } else {
-            null
+        try {
+            val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
+            if (activityManager != null && android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.Q) {
+                @Suppress("DEPRECATION")
+                val tasks = activityManager.getRunningTasks(1)
+                return if (tasks.isNotEmpty()) {
+                    tasks[0].topActivity?.packageName
+                } else {
+                    null
+                }
+            } else {
+                // For Android Q+, use UsageStatsManager instead
+                // This is a simplified fallback - for production, use UsageStatsManager
+                return null
+            }
+        } catch (e: SecurityException) {
+            // Requires GET_TASKS permission which is deprecated
+            return null
+        } catch (e: Exception) {
+            return null
         }
     }
 
