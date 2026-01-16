4e7c232524871c2a7ada3c5d6162d616ada0dced
Guru Swarupa <msgswarupa@gmail.com>
2026-01-16 13:20:03 +0530

    release v4.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.guruswarupa.launchh"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.8"
+        versionCode = 5
+        versionName = "4.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
