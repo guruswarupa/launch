ac4c777ea35526bc504712998adc00f1d97b6ee4
Guru Swarupa <msgswarupa@gmail.com>
2026-02-21 20:11:50 +0530

    fix widget bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.guruswarupa.launch"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "5.8"
+        versionCode = 15
+        versionName = "5.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
